e5ed743f79dd4d36f9442c72cd7a2b2d92fd8ec5
Myles Grant <myles@mylesgrant.com>
2012-02-07 10:24:04 -0800

    Start a listener on the tivo server, add some comments and config options

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
-var listen_host = "localhost";
-var listen_port = 8080;
+/*
+ *
+ * Node-Tivo: An implementation of the Tivo protocol(s) in Node.js
+ * By: Myles Grant <myles@mylesgrant.com>
+ *
+*/
+
+//
+// Configuration options
+//
+
+var web_port = 8080; // Start a web server on this port
+var tivo_port = 2190; // You probably don't want to change this
+
+
+//
+// Start web server
+//
 
 var http = require('http');
 http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end("Hello. Node-Tivo is listening.");
-}).listen(listen_port, listen_host);
+}).listen(web_port, function() { //'listening' listener
+	console.log('Now listening on web port: '+web_port);
+});
+
+
+//
+// Start a tivo server
+//
+
+var net = require('net');
+net.createServer(function(c) { //'connection' listener
+	console.log('Tivo server connection');
+	c.on('end', function() {
+		console.log('Tivo server disconnect');
+	});
+	c.write('hello\r\n');
+	c.pipe(c);
+}).listen(tivo_port, function() { //'listening' listener
+	console.log('Now listening on tivo port: '+tivo_port);
+});
